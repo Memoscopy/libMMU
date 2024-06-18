437fd3ba7128d1f428c9cedd34d69b4a0481485d
std3 <67806187+standard3@users.noreply.github.com>
2024-06-18 16:56:43 +0200

    feat: created TryFromPath and FromPath traits

--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,28 @@
-pub mod configuration;
+use anyhow::Result;
+use std::path::{Path, PathBuf};
+
+/// Try to convert from a path.
+pub trait TryFromPath {
+    /// Converts from a path reference.
+    fn from_path(path: &Path) -> Result<Self>
+    where
+        Self: Sized;
+}
+
+/// Defines a trait for converting from a path.
+pub trait FromPath {
+    /// Converts from a path reference.
+    fn from_path(path: &Path) -> Self;
+}
+
+impl FromPath for String {
+    fn from_path(path: &Path) -> String {
+        path.to_string_lossy().into_owned()
+    }
+}
+
+impl FromPath for PathBuf {
+    fn from_path(path: &Path) -> PathBuf {
+        path.to_path_buf()
+    }
+}
