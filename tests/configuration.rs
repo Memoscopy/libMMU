f67ed6ab7d7b574af8b5c0e01b26caec70ddb98a
std3 <67806187+standard3@users.noreply.github.com>
2024-06-19 16:30:58 +0200

    fix: bad memory regions

--- tests/configuration.rs
+++ tests/configuration.rs
@@ -42,14 +42,14 @@ mod configuration_tests {
             machine.memory_regions.regions[0].region_type,
             MemoryRegionType::RAM
         );
-        assert_eq!(machine.memory_regions.regions[0].start_address, 0x12345678);
-        assert_eq!(machine.memory_regions.regions[0].end_address, 0x87654321);
+        assert_eq!(machine.memory_regions.regions[0].start_address, 0);
+        assert_eq!(machine.memory_regions.regions[0].end_address, 1);
         assert_eq!(
             machine.memory_regions.regions[1].region_type,
             MemoryRegionType::ROM
         );
-        assert_eq!(machine.memory_regions.regions[1].start_address, 0);
-        assert_eq!(machine.memory_regions.regions[1].end_address, 73727);
+        assert_eq!(machine.memory_regions.regions[1].start_address, 2);
+        assert_eq!(machine.memory_regions.regions[1].end_address, 3);
         assert_eq!(
             machine.dumpfile,
             PathBuf::from("tests/fixtures/riscv-sv32.dump")
