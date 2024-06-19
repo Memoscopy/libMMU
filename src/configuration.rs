297e7726648860780db17d3c30e5aea510c93d8c
std3 <67806187+standard3@users.noreply.github.com>
2024-06-19 10:11:55 +0200

    fix: implement from_path to every machine

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,23 @@
 use anyhow::Result;
+use serde::de::DeserializeOwned;
 use std::fs;
 use std::path::Path;
 
 use crate::utils::TryFromPath;
 
-use super::architecture::generic::Machine;
+use super::architecture::generic::{Machine, CPU, MMU};
 
 /// Represents a configuration file for a machine.
 /// It holds information about the machine's architecture, memory regions, and other relevant information.
 /// The configuration file must be written in the TOML format.
-impl TryFromPath for Machine {
+impl<T, U> TryFromPath for Machine<T, U>
+where
+    T: CPU + DeserializeOwned,
+    U: MMU + DeserializeOwned,
+{
     fn from_path(path: &Path) -> Result<Self> {
         let configuration = fs::read_to_string(path)?;
-        let machine: Machine = toml::from_str(&configuration)?;
+        let machine: Machine<T, U> = toml::from_str(&configuration)?;
 
         Ok(machine)
     }
